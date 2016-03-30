315f800468b924ceb67724613c3e64edf993ba74
daeniz <dennis@schmock.eu>
2016-03-30 10:17:26 +0200

    Add frontcontrol

---
+++ src/java/Model/Controller.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author dennisschmock
+ */
+public class Controller {
+    
+    /**
+     * THis method takes two numbers and adds them up...
+     * @param a an integer of any size
+     * @param b an integer of any size
+     * @return the two numbers added
+     */
+    public int addNumbers(int a, int b){
+        return a+b; //here we do something....
+    }
+}
