7864ea9785a10bd3ddf5d9b5ef4dafe2e2fb66e9
Cherry Rose Semena <cherryrosesemena1102@gmail.com>
2016-03-30 11:35:40 +0200

    Control Classes

--- src/java/Control/Building.java
+++ src/java/Control/Building.java
@@ -5,13 +5,84 @@
  */
 package Control;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author dennisschmock
  */
 public class Building {
-    
-    public Building(){
-        
+    private int bdgId;
+    private String bdgName;
+    private String bdgAddress;
+    private int postNum;
+    private int bdgAge;
+    private double bdgSize;
+    private String useOfBdg;
+    private ArrayList<Report> listOfReports;
+    private int custId;
+
+    /**
+     *
+     * @param bdgId   building's ID
+     * @param bdgName building name
+     * @param bdgAddress building address
+     * @param postNum post number
+     * @param bdgAge  building age
+     * @param bdgSize building size
+     * @param useOfBdg the use of the building
+     * @param custId the customer's ID
+     */
+    public Building(int bdgId,String bdgName,
+            String bdgAddress, int postNum, int bdgAge, double bdgSize, 
+            String useOfBdg, int custId) {
+        this.bdgId = bdgId;
+        this.bdgName = bdgName;
+        this.bdgAddress = bdgAddress;
+        this.postNum = postNum;
+        this.bdgAge = bdgAge;
+        this.bdgSize = bdgSize;
+        this.useOfBdg = useOfBdg;
+        this.custId = custId;
+    }
+
+    public int getBdgId() {
+        return bdgId;
+    }
+
+    public String getBdgName() {
+        return bdgName;
+    }
+
+    public String getBdgAddress() {
+        return bdgAddress;
+    }
+
+    public int getPostNum() {
+        return postNum;
+    }
+
+    public int getBdgAge() {
+        return bdgAge;
+    }
+
+    public double getBdgSize() {
+        return bdgSize;
+    }
+
+    public String getUseOfBdg() {
+        return useOfBdg;
+    }
+
+    public ArrayList<Report> getListOfReports() {
+        return listOfReports;
+    }
+
+    public int getCustId() {
+        return custId;
+    }
+
+    public void setListOfReports(ArrayList<Report> listOfReports) {
+        this.listOfReports = listOfReports;
     }
 }
---
+++ src/java/Control/Report.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Control;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CJS
+ */
+public class Report {
+    private int reportNum;
+    private String date;
+    private String bdgName;
+    private String bdgAddress;
+    private int postNum;
+    private int bdgAge;
+    private double bdgSize;
+    private String useOfBdg;
+    private ArrayList<ReportRoom> listOfRepRoom;
+    private int bdgId;
+
+    /**
+     *
+     * @param reportNum  report number
+     * @param date   date
+     * @param bdgName  building name
+     * @param bdgAddress   building address
+     * @param postNum    post number
+     * @param bdgAge    building age
+     * @param bdgSize   building size
+     * @param useOfBdg   use of the building
+     * @param bdgId    building's ID
+     */
+    public Report(int reportNum, String date, String bdgName,
+            String bdgAddress, int postNum, int bdgAge, double bdgSize, 
+            String useOfBdg, int bdgId) {
+        this.reportNum = reportNum;
+        this.date = date;
+        this.bdgName = bdgName;
+        this.bdgAddress = bdgAddress;
+        this.postNum = postNum;
+        this.bdgAge = bdgAge;
+        this.bdgSize = bdgSize;
+        this.useOfBdg = useOfBdg;
+        this.bdgId = bdgId;
+    }
+
+    public int getReportNum() {
+        return reportNum;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getBdgName() {
+        return bdgName;
+    }
+
+    public String getBdgAddress() {
+        return bdgAddress;
+    }
+
+    public int getPostNum() {
+        return postNum;
+    }
+
+    public int getBdgAge() {
+        return bdgAge;
+    }
+
+    public double getBdgSize() {
+        return bdgSize;
+    }
+
+    public String getUseOfBdg() {
+        return useOfBdg;
+    }
+
+    public ArrayList<ReportRoom> getListOfRepRoom() {
+        return listOfRepRoom;
+    }
+
+    public int getBdgId() {
+        return bdgId;
+    }
+  
+    public void setListOfRepRoom(ArrayList<ReportRoom> listOfRepRoom) {
+        this.listOfRepRoom = listOfRepRoom;
+    }
+
+
+}
