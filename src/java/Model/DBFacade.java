1cec00dd578e6b559201b0a8b2e21b775db8dfa4
daeniz <dennis@schmock.eu>
2016-03-30 11:23:18 +0200

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	src/java/Model/DBfacade.java

---
+++ src/java/Model/DBFacade.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author dennisschmock
+ */
+public class DBFacade {
+    private Connection con;
+    private static DBFacade instance;
+    
+    private DBFacade() {
+      
+        con = DBconnector.getInstance().getConnection();
+    }
+
+    public static DBFacade getInstance() {
+        if (instance == null) {
+            instance = new DBFacade();
+        }
+        return instance;
+    }
+}
