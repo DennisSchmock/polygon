3be8b9a457dbb83b85851fde4c580773e96e42b9
daeniz <dennis@schmock.eu>
2016-03-31 13:04:47 +0200

    Updated Customer.java and CustomerMapper.java

--- src/java/Model/CustomerMapper.java
+++ src/java/Model/CustomerMapper.java
@@ -18,22 +18,24 @@ import java.sql.Statement;
 public class CustomerMapper {
 
     public void addCustomerToDB(Customer cus, Connection con) {
-        String SQLString = "insert into Report values (?,?,?,?)";
-        try (PreparedStatement statement
+        String SQLString = "insert into Customer values (?,?,?,?)";
+        try (
+                PreparedStatement statement
                 = con.prepareStatement(SQLString, Statement.RETURN_GENERATED_KEYS)) {
-           
+            statement.setString(1, cus.getCompanyName());
+            statement.setString(2, cus.getContactPerson());
+
             int rowsInserted = statement.executeUpdate();
             ResultSet rs = statement.getGeneratedKeys();
         } catch (Exception e) {
             System.out.println("Fail in saving new report - saveNewReport");
             System.out.println(e.getMessage());
         }
-        
-        
+
     }
-    
-    public void editCustomer(){
-        
+
+    public void editCustomer() {
+
     }
-    
+
 }
