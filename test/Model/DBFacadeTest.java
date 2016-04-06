3a311ddcfa5a721703781cf20cf827ffddd3c59a
Cherry Rose Semena <cherryrosesemena1102@gmail.com>
2016-04-06 12:55:21 +0200

    CustomerMapperTest

--- test/Model/DBFacadeTest.java
+++ test/Model/DBFacadeTest.java
@@ -33,22 +33,55 @@ public class DBFacadeTest {
         dbf.setCon(fixture.getConnection());
     }
 
-    @Test
-    public void testMain() {
-    }
+//    @Test
+//    public void testMain() {
+//    }
 
 //    @Test
 //    public void testGetInstance() {
 //    }
 //
-    @Test
-    public void testValidateUser() {
-        dbf.createUserDBFacade(null);
-    }
+//    @Test
+//    public void testValidateUser() {
+//    }
     
+    /**
+     * Test to see if a user is created the right way.
+     * Also needs to run the load method for loading to check if its right 
+     * 
+     */
     @Test
-    public void createUserDBFacade() {
+    public void testCreateUserDBFacadeAndLoad() {
+        String usernameexpected ="Danielh";
+        String Passwordexpected="123";
+        int customerIDexpected = 1;
+        String fNameexpected= "Daniel";
+        String lNameexpected= "Hollmann";
+        String emailexpected = "danielhollmann@hotmail.com";
+        String phoneexpected = "22878265";
+        String companyNameexpected = "Redskins";
+
+        User user = new User(usernameexpected,Passwordexpected,customerIDexpected,fNameexpected,lNameexpected,emailexpected,phoneexpected,companyNameexpected);
+        dbf.createUserDBFacade(user);
         
+        User actualUser = dbf.loadUser(usernameexpected);
+        String usernameactual = actualUser.getUserName();
+        String Passwordactual= actualUser.getPassword();
+        int customerIDactual = actualUser.getCustomerid();
+        String fNameactual= actualUser.getfName();
+        String lNameactual= actualUser.getlName();
+        String emailactual = actualUser.getEmail();
+        String phoneactual = actualUser.getPhone();
+        String companyNameactual = actualUser.getCompanyName();
+        
+        assertTrue("Fail, Expeted: " +usernameexpected + " Found: " + usernameactual,usernameexpected.equals(usernameactual));
+        assertTrue("Fail, Expeted: " +Passwordexpected + " Found: " + Passwordactual,Passwordexpected.equals(Passwordactual));
+        assertTrue("Fail, Expeted: " +customerIDexpected + " Found: " + customerIDactual,customerIDexpected == customerIDactual);
+        assertTrue("Fail, Expeted: " +fNameexpected + " Found: " + fNameactual,fNameexpected.equals(fNameactual));
+        assertTrue("Fail, Expeted: " +lNameexpected + " Found: " + lNameactual,lNameexpected.equals(lNameactual));
+        assertTrue("Fail, Expeted: " +emailexpected + " Found: " + emailactual,emailexpected.equals(emailactual));
+        assertTrue("Fail, Expeted: " +phoneexpected + " Found: " + phoneexpected,phoneexpected.equals(phoneactual));
+        assertTrue("Fail, Expeted: " +companyNameexpected + " Found: " + companyNameactual,companyNameexpected.equals(companyNameactual));
     }
 //
 //    @Test
@@ -147,8 +180,5 @@ public class DBFacadeTest {
 //    public void testUpdateBuildingDBFacade() {
 //    }
 //
-    @Test
-    public void testLoadUser() {
-    }
     
 }
