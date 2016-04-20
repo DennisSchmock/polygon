0c0267db0cfb06ae373f6cd19798ce071b275762
Danielgron <danielgronbjerg@gmail.com>
2016-04-20 13:46:42 +0200

    Merge origin/master
    Conflicts:
    	src/java/Control/FrontControl.java

--- src/java/Control/PrinterPDF.java
+++ src/java/Control/PrinterPDF.java
@@ -41,7 +41,7 @@ public class PrinterPDF {
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PolygonException {
         //Test class:
         Report report=null;
         try {
