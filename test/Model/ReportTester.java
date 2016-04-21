4db4e064a3dc1b36eaad07cd1f059379f70cd0dc
Danielh21 <danielhollmann@hotmail.com>
2016-04-21 10:48:16 +0200

    Set up for report test

--- test/Model/ReportTester.java
+++ test/Model/ReportTester.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import Domain.*;
+import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 /**
@@ -22,6 +23,7 @@ public class ReportTester {
     
         DBFixture fixture;
         DBFacade dbf;
+        Report r;
     
     public ReportTester() {
     }
@@ -43,6 +45,7 @@ public class ReportTester {
         Connection con =fixture.getConnection();
         con.setAutoCommit(true);
         dbf.setCon(con);
+        reportSetup();
     }
     
     @After
@@ -50,20 +53,61 @@ public class ReportTester {
          fixture.closeConnection();
     }
     
-    /** Method for testing the database save report and load report.
+    /** Method for Setting up an Report object, that can be altered and used
+     * for testing purpurse in other testmethods
      * There is not need for creating id's for the different report elements
      * because the database should take care of that
      *
      */
-    @Test
-    public void reportTester(){
+    public void reportSetup(){
         Report report = new Report(1, "emp1");
-        ReportExterior reportExteior;
-        ReportRoom room;
-        ReportRoomDamage roomdmg;
-        ReportRoomInterior roomInterior;
-        ReportRoomMoist roomMoist;
-        ReportRoomRecommendation roomRecomendation;
+        
+        // Exterior
+        ArrayList<ReportExterior> listReportEx = new  ArrayList<>();
+        ReportExterior reportExteior1 = new ReportExterior("Testing area","Testing Decription");
+        listReportEx.add(reportExteior1);
+        report.setListOfRepExt(listReportEx);
+        
+        // Room1
+        ReportRoom room1 = new ReportRoom("Testing Room Name 1", 1); // 1 is the foreign key to the test building Room already in the database
+        
+        
+        //Interior
+        ArrayList<ReportRoomInterior> listReportint = new  ArrayList<>();
+        ReportRoomInterior roomInterior1 = new ReportRoomInterior("Test Report Romm Area", "Test Remark");
+        ReportRoomInterior roomInterior2 = new ReportRoomInterior("Test Report Room 2", "Test Remark 2");
+        listReportint.add(roomInterior1);
+        listReportint.add(roomInterior2);
+        room1.setListOfInt(listReportint);
+        
+        
+        //Room Damage:
+        ArrayList<ReportRoomDamage> listofRoomDmg = new  ArrayList<>();
+        ReportRoomDamage roomdmg1 = new ReportRoomDamage("Test Time", "Test Place", "Test what happend", "Test Type", "Test Repaired");
+        listofRoomDmg.add(roomdmg1);
+        room1.setListOfDamages(listofRoomDmg);
+        
+        //Moist
+        ReportRoomMoist moist1 = new ReportRoomMoist("Test measured", "Test Area");
+        room1.setMoist(moist1);
+        
+        // Recomendations
+        ArrayList<ReportRoomRecommendation> listofRecomendations = new ArrayList<>();
+        ReportRoomRecommendation rec1 = new ReportRoomRecommendation("Test Recomendation");
+        listofRecomendations.add(rec1);
+        room1.setListOfRec(listofRecomendations);
+        
+        ArrayList<ReportRoom> roomlist = new ArrayList<>();
+        roomlist.add(room1);
+        roomlist.add(room1); // Places to identically rooms to the object
+        report.setListOfRepRoom(roomlist);
+        System.out.println(report.toString());
+        
+    }
+    
+    @Test
+    public void testReport(){
+        
     }
     
 }
